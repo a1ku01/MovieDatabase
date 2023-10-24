09ee51a2dd1eb6f4d766ce1203abc874ae8fd233
Svetlana Danilina <s.danilina636@gmail.com>
2023-10-24 14:14:48 +0300

    Movie images added in HomeView, Database is updated

---
+++ vue-project/src/constants.js
@@ -0,0 +1,14 @@
+export const movieImageUrls = {
+    // key-value map
+    // key = uuid
+    // value = image url
+    '7d06dff4-aa68-4a78-817a-16d2dee204f2': 'https://m.media-amazon.com/images/I/613ypTLZHsL._AC_UF1000,1000_QL80_.jpg',
+    'f2cfd271-8245-4fbb-be22-c8d7d084073e': 'https://upload.wikimedia.org/wikipedia/en/b/ba/Poster_-_The_Matrix_Reloaded.jpg',
+    '7bd28eb4-f128-4ffd-bf9e-2dfed08911d2': 'https://upload.wikimedia.org/wikipedia/en/3/34/Matrix_revolutions_ver7.jpg',
+    '8082ed08-1e79-4d96-9e38-2154ab80f898': 'https://upload.wikimedia.org/wikipedia/en/e/ed/The-internship-poster.jpg',
+    'cdade04f-c2e7-4379-bfba-20ccafc37457': 'https://m.media-amazon.com/images/S/pv-target-images/61193ac3f8852ad102f0eec91097539c49b34f715444d644bb3165ddad4a934f.jpg',
+    'eecd0f15-9263-40f1-9351-8aabf179bb50': 'https://m.media-amazon.com/images/M/MV5BOGUyZDUxZjEtMmIzMC00MzlmLTg4MGItZWJmMzBhZjE0Mjc1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg',
+    'ce3a18e4-b15a-4f74-9402-3c8b0814db5c': 'https://m.media-amazon.com/images/I/81Zc1Xgz7zL._AC_UF1000,1000_QL80_.jpg',
+    '20976a0a-172a-45bf-90a1-0ad88396cfac': 'https://m.media-amazon.com/images/M/MV5BMTg2MzYzNzgzOF5BMl5BanBnXkFtZTgwOTg4NzQ4OTE@._V1_.jpg',
+    '831d1722-c350-48b0-b06d-65fe0150e662': 'https://m.media-amazon.com/images/I/81f0DSetYvL._AC_UF1000,1000_QL80_.jpg',
+}
